979c8bead87467ecfc76ba545b2ac0f21a2e5d01
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-10 04:03:45 +0600

    validation done

---
+++ validation.js
@@ -0,0 +1,17 @@
+// Validation Or Check Input
+/* function add(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'Please Enter a Number';
+    }
+    return num1 + num2;
+}
+const result = add(127, 45);
+console.log(result); */
+
+// Error Handle
+function multipy(num1, num2) {
+    console.log(num1, num2);
+    return num1 * num2;
+}
+const output = multipy(12 * 45);
+console.log(output);
